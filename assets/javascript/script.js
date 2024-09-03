874e85bc8ee63a950a3337bad893007f9dd0f742
ojiiis <official.ojingirisamuel@gmail.com>
2024-09-03 13:09:16 +0100

    03-09-2024

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -10,17 +10,35 @@ for(i of vmi){
     if(document.getElementById(elem))
     document.getElementById(elem).style.visibility = "hidden";
  }
- 
 
- var vms =  document.getElementsByClassName("vendor-menu-lister")[0],vmsc = vms.children.length,vmsi = 0;
+ 
+const cqi = document.getElementsByClassName('cart-qty');
+for(let i of cqi){
+    i.parentElement.parentElement.children[2].innerText = Intl.NumberFormat().format(i.id * i.value)
+    i.onchange = function(){
+        i.parentElement.parentElement.children[2].innerText = Intl.NumberFormat().format(i.id * i.value)
+        document.getElementById('subtotal').innerText = new Intl.NumberFormat().format(sumCart());
+    }
+    document.getElementById('subtotal').innerText = new Intl.NumberFormat().format(sumCart());
+}
+
+function sumCart(){
+    const cqi = document.getElementsByClassName('cart-qty');
+   let total = 0
+    for(let i of cqi){
+        total += i.id * i.value
+    }  
+    return total; 
+}
+ var vms =  document.getElementsByClassName("vendor-menu-lister")[0],vmsc = vms?.children.length,vmsi = 0;
  setInterval(function(){
    
     if(vmsi >= (vmsc - 1)){
         vmsi = 0;
         vms.scrollTo(0,0);
     }else{
-    let goto = vms.children[vmsi+1].offsetLeft - 20;
-    vms.scrollTo(goto,0);
+    let goto = vms?.children[vmsi+1].offsetLeft - 20;
+    vms?.scrollTo(goto,0);
     vmsi++;
     }
 
